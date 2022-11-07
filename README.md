b292e85e2ffc173160bdcde8607f8836c877436d
nealcormier <cormier.neal@gmail.com>
2022-11-07 16:26:52 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-zsyify-pfx17b
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-zsyify-pfx17b)
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  products = products;
+
+  share() {
+    window.alert('You share this!');
+  }
+
+  onNotify() {
+    window.alert('You will be notified asshole');
+  }
+
+
+}
+
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
